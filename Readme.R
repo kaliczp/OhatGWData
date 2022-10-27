90ded5400031cb84182dbbf9cd425bce22a564ae
Péter Kalicz <kaliczp@gmail.com>
2022-10-27 17:14:24 +0200

    Missing dataset is added
    A hiányzó adatsort közben megtaláltam és hozzáadtam.
    
    Adtam egy címet a műveleteknek.

--- Readme.R
+++ Readme.R
@@ -1,4 +1,6 @@
 ## https://github.com/kaliczp/smartbe függvényét használva
+
+## Beolvasás
 ohat121a09 <- smartbe("2021/Ohat1a09.TXT")
 ohat121b27 <- smartbe("2021/Ohat1b27.TXT")
 ohat121b27[2,2] <- NA
@@ -8,5 +10,6 @@ ohat122414 <- smartbe("2022/Ohat1414.TXT")
 ohat122414mod <- ohat122414
 ohat122414mod[c(1,nrow(ohat122414mod)),2] <- NA
 ohat122414mod$Measure <- ohat122414mod$Measure + 0.157
+ohat122625 <- smartbe("2022/Ohat1625.TXT")
 ohat122a21 <- smartbe("2022/Ohat1a21.TXT")
 ohat1 <- rbind(ohat121a09, ohat121b27,ohat121c28,ohat122414mod,ohat122a21)
