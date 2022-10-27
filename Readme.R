6ebc039918416ba84859efc3cc0016e4719c40f4
Péter Kalicz <kaliczp@gmail.com>
2022-10-27 16:34:17 +0200

    Initial commit
    Meglévő adatok beolvasása data.frame-be.

---
+++ Readme.R
@@ -0,0 +1,12 @@
+## https://github.com/kaliczp/smartbe függvényét használva
+ohat121a09 <- smartbe("2021/Ohat1a09.TXT")
+ohat121b27 <- smartbe("2021/Ohat1b27.TXT")
+ohat121b27[2,2] <- NA
+ohat121c28 <- smartbe("2021/Ohat1c28.TXT")
+ohat121c28[1,2] <- NA
+ohat122414 <- smartbe("2022/Ohat1414.TXT")
+ohat122414mod <- ohat122414
+ohat122414mod[c(1,nrow(ohat122414mod)),2] <- NA
+ohat122414mod$Measure <- ohat122414mod$Measure + 0.157
+ohat122a21 <- smartbe("2022/Ohat1a21.TXT")
+ohat1 <- rbind(ohat121a09, ohat121b27,ohat121c28,ohat122414mod,ohat122a21)
