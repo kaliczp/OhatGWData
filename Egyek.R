c3dbaae280b6f66d445bd3002dd59dd9896fab12
Péter Kalicz <kaliczp@gmail.com>
2022-11-02 22:35:33 +0100

    Temperature and humidity processing

--- Egyek.R
+++ Egyek.R
@@ -15,3 +15,23 @@ napi.csap <- apply.daily(csap.xts, function(x){sum(x, na.rm = TRUE)})
 havi.csap <- apply.monthly(csap.xts, function(x){sum(x, na.rm = TRUE)})
 write.zoo(napi.csap, "Egyek/csap.csv", sep = ";", dec = ",")
 write.zoo(havi.csap, "Egyek/havicsap.csv", sep = ";", dec = ",")
+
+## Hőmérséklet és pára
+homfilenames <- filenames[2:13]
+ttraw <- read.csv2(paste0("Egyek/", homfilenames[2]))
+## Hőmérséklet
+hom <- ttraw[ttraw$parameter == "Levegőhőmérséklet",]
+## Pára
+para <- ttraw[ttraw$parameter == "Relatív páratartalom",]
+## Az összes fájlra
+for(tti in 2:length(homfilenames)) {
+ttraw <- read.csv2(paste0("Egyek/", homfilenames[tti]))
+hom <- rbind(hom, ttraw[ttraw$parameter == "Levegőhőmérséklet",])
+para <- rbind(para, ttraw[ttraw$parameter == "Relatív páratartalom",])
+}
+## Dupla dátum kiszedése
+hom <- hom[!duplicated(hom$date),]
+para <- para[!duplicated(para$date),]
+## Idősor
+hom.xts <- xts(hom$value, as.POSIXct(hom$date))
+para.xts <- xts(para$value, as.POSIXct(para$date))
