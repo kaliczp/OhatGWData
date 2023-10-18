db2f6cfddaa85ea4586e31bb2827f31ed4aaf81d
Péter Kalicz <kaliczp@gmail.com>
2023-10-18 13:24:09 +0200

    Ohat2 white test

---
+++ Ohat2White.R
@@ -0,0 +1,7 @@
+oh2.w <- White(ohat2jav.xts['2023-08-03 23:50:00/2023-08-08'], Sy = 0.134)
+oh2.w <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-15'], Sy = 0.134)
+
+plot.White(oh2.w)
+timeaxtics <- seq(starttime, endtime + 24*60*60, by = "day")
+
+## Talán a plot van eggyel eltolva? csak a meredekség?
