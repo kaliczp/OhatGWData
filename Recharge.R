b2260fb2231581737f69c116d05bad6196290505
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-25 13:51:04 +0200

    Preparation of selection

--- Recharge.R
+++ Recharge.R
@@ -1,8 +1,9 @@
 library(readxl)
 csapsel <- as.data.frame(read_excel("Egyek/csapössz.xlsx", 1))
+csapsel.xts <- xts(csapsel[,4], csapsel[,1])
 
 oh2rech.w <- White(ohat2jav.xts['2022-04-14 23:50:00/2023-08-31'], Sy = 0.134)
-oh2rech.w <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-15'], Sy = 0.134)
 
-plot.White(oh2.w)
-head(oh2rech.w$results)
+oh2rech.w <- White(ohat2jav.xts['2023-08-01/2023-08-31'], Sy = 0.134)
+plot.White(oh2rech.w)
+oh2.slope <- oh2rech.w$results[,1]
