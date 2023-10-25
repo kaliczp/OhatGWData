b20504fb0487c5311b0e8db5b70779cb4176527b
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-25 13:32:40 +0200

    OK teszt

--- Recharge.R
+++ Recharge.R
@@ -1,2 +1,8 @@
 library(readxl)
-csapsel <- as.data.frame(read_excel("Egyek/csapössz.xlsx"))
+csapsel <- as.data.frame(read_excel("Egyek/csapössz.xlsx", 1))
+
+oh2rech.w <- White(ohat2jav.xts['2022-04-14 23:50:00/2023-08-31'], Sy = 0.134)
+oh2rech.w <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-15'], Sy = 0.134)
+
+plot.White(oh2.w)
+head(oh2rech.w$results)
