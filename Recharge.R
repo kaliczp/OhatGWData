c70db84d1be22ae83c0267b18394dd59266b58a2
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-25 13:55:55 +0200

    Working selection of slopes

--- Recharge.R
+++ Recharge.R
@@ -4,6 +4,9 @@ csapsel.xts <- xts(csapsel[,4], csapsel[,1])
 
 oh2rech.w <- White(ohat2jav.xts['2022-04-14 23:50:00/2023-08-31'], Sy = 0.134)
 
-oh2rech.w <- White(ohat2jav.xts['2023-08-01/2023-08-31'], Sy = 0.134)
+intervallum <- '2023-08-01/2023-08-31'
+oh2rech.w <- White(ohat2jav.xts[intevallum], Sy = 0.134)
 plot.White(oh2rech.w)
 oh2.slope <- oh2rech.w$results[,1]
+select <- !coredata(csapsel.xts[intervallum])[,1]
+oh2.slope[select]
