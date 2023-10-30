812e11c8a49202b29fc46f25a11e2accbe2ef4f3
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-30 10:15:43 +0100

    Automata összerakás kísérlet

--- Recharge.R
+++ Recharge.R
@@ -11,8 +11,6 @@ intervallum <- '2022-05-01/2022-06-24'
 intervallum <- '2022-06-26/2022-08-31'
 ## '2021-11-01/2022-02-28' adathiányig
 intervallum <- '2021-11-01/2021-11-26'
-intervallum <- '2021-11-01/2021-12-2u'
-
 intervallum <- '2021-11-30/2021-12-27'
 
 oh2rech.w <- White(ohat2jav.xts[intervallum], Sy = 0.134)
@@ -20,3 +18,27 @@ plot.White(oh2rech.w)
 oh2.slope <- oh2rech.w$results[,1]
 select <- !coredata(csapsel.xts[intervallum])[,1]
 oh2.slope[select]
+
+intervallumok <- c('2023-06-25/2023-08-31'
+,'2023-05-01/2023-06-23'
+,'2022-12-22/2023-03-01'
+,'2022-11-01/2022-12-20'
+## 2022-05-01/2022-08-31
+,'2022-05-01/2022-06-24'
+,'2022-06-26/2022-08-31'
+## '2021-11-01/2022-02-28' adathiányig
+,'2021-11-01/2021-11-26'
+,'2021-11-30/2021-12-27'
+)
+
+oh2rech.w <- White(ohat2jav.xts[intervallumok[1]], Sy = 0.134)
+oh2.slope <- oh2rech.w$results[,1]
+select <- !coredata(csapsel.xts[intervallumok[1]])[,1]
+slope.xts <- oh2.slope[select]
+
+for(intervallum in intervallumok[-1]){ 
+    oh2rech.w <- White(ohat2jav.xts[intervallum], Sy = 0.134)
+    oh2.slope <- oh2rech.w$results[,1]
+    select <- !coredata(csapsel.xts[intervallum])[,1]
+    slope.xts <- c(slope.xts, oh2.slope[select])
+}
