ba84f31930cfa6457c2875d5a1f310e599b50071
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-17 12:06:31 +0100

    Monthly average calculated

--- Readme.R
+++ Readme.R
@@ -153,3 +153,8 @@ write.zoo(daily.amp.date, "Ohat1/ampli.csv", sep = ";", dec = ",")
 daily.mean <- apply.daily(ohat1.xts, mean)
 daily.mean.date <- xts(coredata(daily.mean), as.Date(index(daily.mean)))
 write.zoo(daily.mean.date, "Ohat1/mean.csv", sep = ";", dec = ",")
+
+## Monthly average
+monthly.mean <- apply.monthly(ohat1.xts['/2023-08'], function(x){mean(x, na.rm = TRUE)})
+monthly.mean.date <- xts(coredata(monthly.mean), as.Date(index(monthly.mean)))
+write.zoo(monthly.mean.date, "Ohat1/MonthlyMean.csv", sep = ";", dec = ",")
