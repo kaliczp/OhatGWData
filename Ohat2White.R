dd551c3dbd22ab545669a4b86d9016485fe97209
Kele Zsombor <kele.zsombor48@gmail.com>
2024-02-18 22:00:24 +0000

    Apró vált

--- Ohat2White.R
+++ Ohat2White.R
@@ -4,7 +4,7 @@ oh2.w2 <- White(ohat2jav.xts['2023-08-03 23:50:00/2023-08-08'], Sy = 0.134)
 oh2.w2 <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-31'], Sy = 0.134)
 oh2.w2 <- White(ohat2jav.xts['2022-05-01 23:50:00/2022-06-30'], Sy = 0.134)
 
-
+getwd("Relativ paratart")
 write.zoo(oh2.w2$results, "GW_ET.csv", sep = ";", dec = ".")
 
 
