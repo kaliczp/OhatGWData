1d060342aef3eb6d6ad05d1d7b9ea858ba832b47
Kalicz Péter <kaliczp@gmail.com>
2024-02-19 12:58:47 +0100

    Initial time-series generation for humidity

--- TalajnedvPara.R
+++ TalajnedvPara.R
@@ -7,3 +7,11 @@ data_Görbehaza <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talaj
 data_Görbehaza <- rbind(data_Görbehaza, read.csv2("Talajnedv_paratart/2023-07-01-08-31-Paratart_talajnedv_Görbehaza.txt.txt", dec = "."))
 data_Szentistvan <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Szentistvan.txt.txt", dec = ".")
 data_Szentistvan <- rbind(data_Szentistvan, read.csv2("Talajnedv_paratart/2023-07-01-08-31-Paratart_talajnedv_Szentistvan.txt.txt", dec = "."))
+
+## Relatív páratartalom
+library(xts)
+Humidity.xts <- xts(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "value"], as.POSIXct(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "date"]))
+plot(Humidity.xts)
+plot(Humidity.xts['2023'])
+plot(Humidity.xts['2023-07'])
+plot(Humidity.xts['2023-07-10'])
