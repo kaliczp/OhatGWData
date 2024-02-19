5d9348f9cc7ab3b93216fefcdb12ee86d1bb17c2
Kalicz Péter <kaliczp@gmail.com>
2024-02-19 12:24:55 +0100

    Soilmoisture and rel huidity import initial step
    Kimásoltam az import sorokat és próbálom hasonló nevezéktant használni. Mindent hagytam az elnevezett könyvtárban. Ezt létre kell hozni és a fájlokat bemásolni. A .gitignoreba betettem az új mappát.

---
+++ TalajnedvPara.R
@@ -0,0 +1,5 @@
+## Hagytam a Talaned_paratart mappában, amiben adtad. Csinálni kell és áttenni a fájlokat.
+data_Egyek <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Egyek.txt.txt", dec = ".")
+data_Kunmadaras <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Kunmadaras.txt.txt", dec = ".")
+data_Görbehaza <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Görbehaza.txt.txt", dec = ".")
+data_Szentistvan <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Szentistvan.txt.txt", dec = ".")
