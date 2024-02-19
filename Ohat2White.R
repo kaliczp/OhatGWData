ddd82d8fcbe5cc7df0dabcd593f4bbee956e21fe
Kele Zsombor <kele.zsombor48@gmail.com>
2024-02-19 12:57:27 +0000

    Apró változások

--- Ohat2White.R
+++ Ohat2White.R
@@ -1,10 +1,15 @@
 oh2.w2 <- White(ohat2jav.xts['2023-05-01 23:50:00/2023-07-08'], Sy = 0.085)
+oh2all.xts <- oh2.w2$results
 oh2.w2 <- White(ohat2jav.xts['2023-07-08 23:50:00/2023-08-03'], Sy = 0.085)
+oh2all.xts <- c(oh2all.xts, oh2.w2$results)
 oh2.w2 <- White(ohat2jav.xts['2023-08-03 23:50:00/2023-08-08'], Sy = 0.085)
+oh2all.xts <- c(oh2all.xts, oh2.w2$results)
 oh2.w2 <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-31'], Sy = 0.085)
+oh2all.xts <- c(oh2all.xts, oh2.w2$results)
 oh2.w2 <- White(ohat2jav.xts['2022-05-01 23:50:00/2022-06-30'], Sy = 0.085)
+oh2all.xts <- c(oh2all.xts, oh2.w2$results)
 
-write.zoo(oh2.w2$results, "GW_ET.csv", sep = ";", dec = ".")
+write.zoo(oh2all.xts, "GW_ET.csv", sep = ";", dec = ".")
 
 
 plot.White(oh2.w2)
--- TalajnedvPara.R
+++ TalajnedvPara.R
@@ -1,4 +1,4 @@
-## Hagytam a Talaned_paratart mappában, amiben adtad. Csinálni kell és áttenni a fájlokat.
+## Hagytam a Talajnedv_paratart mappában, amiben adtad. Csinálni kell és áttenni a fájlokat.
 data_Egyek <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Egyek.txt.txt", dec = ".")
 data_Egyek <- rbind(data_Egyek, read.csv2("Talajnedv_paratart/2023-07-01-08-31-Paratart_talajnedv_Egyek.txt.txt", dec = "."))
 data_Kunmadaras <- read.csv2("Talajnedv_paratart/2022-05-01-06-30-Paratart_talajnedv_Kunmadaras.txt.txt", dec = ".")
@@ -10,8 +10,13 @@ data_Szentistvan <- rbind(data_Szentistvan, read.csv2("Talajnedv_paratart/2023-0
 
 ## Relatív páratartalom
 library(xts)
-Humidity.xts <- xts(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "value"], as.POSIXct(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "date"]))
-plot(Humidity.xts)
-plot(Humidity.xts['2023'])
-plot(Humidity.xts['2023-07'])
-plot(Humidity.xts['2023-07-10'])
+Humidity_Egyek.xts <- xts(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "value"], as.POSIXct(data_Egyek[data_Egyek$parameter == "Relatív páratartalom", "date"]))
+plot(Humidity_Egyek.xts)
+plot(Humidity_Egyek.xts['2023'])
+plot(Humidity_Egyek.xts['2023-07'])
+plot(Humidity_Egyek.xts['2023-07-10'])
+write.zoo(Humidity_Egyek.xts, "Humidity_Egyek.csv", sep = ";", dec = ".")
+
+## Talajnedvesség -- ez nem működik -- valahogy bele kéne rakni a talajnedv.xts-be a station.name-t is, hogy utána tudjunk négy különböző görbét rajzolni
+Talajnedv.xts <- xts(data_Egyek[data_Egyek$parameter == "Talajnedvesség", "value", "station.name"], as.POSIXct(data_Egyek[data_Egyek$parameter == "Talajnedvesség", "date"]))
+plot(Talajnedv.xts))
