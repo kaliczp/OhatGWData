2edf4aca78a9d8738fe1a78cc7822402ca3f71f9
Kele Zsombor <kele.zsombor48@gmail.com>
2024-02-19 02:51:48 +0000

    Apró változás

--- Ohat2White.R
+++ Ohat2White.R
@@ -1,10 +1,9 @@
-oh2.w2 <- White(ohat2jav.xts['2023-05-01 23:50:00/2023-07-08'], Sy = 0.134)
-oh2.w2 <- White(ohat2jav.xts['2023-07-08 23:50:00/2023-08-03'], Sy = 0.134)
-oh2.w2 <- White(ohat2jav.xts['2023-08-03 23:50:00/2023-08-08'], Sy = 0.134)
-oh2.w2 <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-31'], Sy = 0.134)
-oh2.w2 <- White(ohat2jav.xts['2022-05-01 23:50:00/2022-06-30'], Sy = 0.134)
+oh2.w2 <- White(ohat2jav.xts['2023-05-01 23:50:00/2023-07-08'], Sy = 0.085)
+oh2.w2 <- White(ohat2jav.xts['2023-07-08 23:50:00/2023-08-03'], Sy = 0.085)
+oh2.w2 <- White(ohat2jav.xts['2023-08-03 23:50:00/2023-08-08'], Sy = 0.085)
+oh2.w2 <- White(ohat2jav.xts['2023-08-08 23:50:00/2023-08-31'], Sy = 0.085)
+oh2.w2 <- White(ohat2jav.xts['2022-05-01 23:50:00/2022-06-30'], Sy = 0.085)
 
-getwd("Relativ paratart")
 write.zoo(oh2.w2$results, "GW_ET.csv", sep = ";", dec = ".")
 
 
