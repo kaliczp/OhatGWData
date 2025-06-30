46f5242c23613439c0786068450b70312c99770d
Kalicz Péter <kaliczp@gmail.com>
2025-06-30 14:32:34 +0200

    Automatised plotting
    Now less code rows makes same plot with loops. Color selection by label. Colorlabels unified with data labels to make loops.

--- Fig3GWdaily.R
+++ Fig3GWdaily.R
@@ -8,7 +8,7 @@ names(GW_daily) <- c("Date",
 GW_daily$Date <- as.Date(GW_daily$Date)
 
 GWcolors <- c(
-    ref = "#8d8d8d",
+    reference = "#8d8d8d",
     active = "#00c3ff",
     passive = "#ffd500")
 
@@ -20,41 +20,24 @@ selectedYears <- as.Date(paste0(2000:2010,"-01-01"))
 pdf("Fig3GWdaily.pdf", width = 10/2.54, height = 15/2.54, pointsize = 10)
 par(mfrow = c(3,1), las = 1,
     mar = c(0,4.1,0,0.3), oma = c(2.1,0,2.1,0))
-plot.zoo(GW.akt$shall_reference, ylim = c(-5,1), type = "n",
-         axes = FALSE,
-         xaxs = "i",
-         xlab = "", ylab = "GWL"
-         )
-grid(nx = NA, ny = NULL)
-lines(as.zoo(GW.akt$shall_reference), col = GWcolors["ref"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$shall_active), col = GWcolors["active"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$shall_passive), col = GWcolors["passive"], ylim = c(1,-5), lwd = 2)
-## Axes
-axis(2)
-axis.Date(3, at= c(selectedYears, as.Date("2010-12-31")), labels = FALSE)
-axis.Date(3, at= selectedYears + 365/2, tck = 0, format = "%Y")
-plot.zoo(GW.akt$medi_reference, ylim = c(-5,1), type = "n",
-         axes = FALSE,
-         xaxs = "i",
-         xlab = "", ylab = "GWL"
-         )
-grid(nx = NA, ny = NULL)
-lines(as.zoo(GW.akt$medi_reference), col = GWcolors["ref"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$medi_active), col = GWcolors["active"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$medi_passive), col = GWcolors["passive"], ylim = c(1,-5), lwd = 2)
-## Axes
-axis(2)
-plot.zoo(GW.akt$deep_reference, ylim = c(-5,1), type = "n",
-         axes = FALSE,
-         xaxs = "i",
-         xlab = "", ylab = "GWL"
-         )
-grid(nx = NA, ny = NULL)
-lines(as.zoo(GW.akt$deep_reference), col = GWcolors["ref"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$deep_active), col = GWcolors["active"], ylim = c(1,-5), lwd = 2)
-lines(as.zoo(GW.akt$deep_passive), col = GWcolors["passive"], ylim = c(1,-5), lwd = 2)
-## Axes
-axis(2)
-axis.Date(1, at= c(selectedYears, as.Date("2010-12-31")), labels = FALSE)
-axis.Date(1, at= selectedYears + 365/2, tck = 0, format = "%Y")
+for(gwdepth in c("shall", "medi", "deep")){
+    combiname <-  paste(gwdepth, "reference", sep = "_")
+    plot.zoo(GW.akt[, combiname], ylim = c(-5,1), type = "n",
+             axes = FALSE,
+             xaxs = "i",
+             xlab = "", ylab = "GWL"
+             )
+    grid(nx = NA, ny = NULL)
+    for(gwtype in c("reference", "active", "passive")) {
+        combiname <-  paste(gwdepth, gwtype, sep = "_")
+        lines(as.zoo(GW.akt[, combiname]), col = GWcolors[gwtype], ylim = c(1,-5), lwd = 2)
+    }
+    ## Axes
+    axis(2)
+    if(gwdepth == "shall" || gwdepth == "deep") {
+        axside <- ifelse(gwdepth == "shall", 3, 1)
+        axis.Date(axside, at= c(selectedYears, as.Date("2010-12-31")), labels = FALSE)
+        axis.Date(axside, at= selectedYears + 365/2, tck = 0, format = "%Y")
+    }
+}
 dev.off()
