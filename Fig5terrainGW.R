2c106379c7540d07e7f369e2bc8e95243f4570a2
Kalicz Péter <kaliczp@gmail.com>
2025-07-03 22:31:04 +0200

    Forest refactor in the upper half

--- Fig5terrainGW.R
+++ Fig5terrainGW.R
@@ -10,6 +10,13 @@ names(Terr2) <- c("x","y")
 GW2 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", sheet = 2, range = "F1:P92"))
 GW2 <- GW2[, -c(3:5,9)] # Remove repeated and empty cols
 
+# forest 966–2487
+Forest1 <- Terr1[Terr1[,1] > 1981 & Terr1[,1] < 3600,]
+Forest1poly <- rbind(Forest1 + data.frame(rep(0,nrow(Forest1)), rep(1, nrow(Forest1))),
+                     Forest1[nrow(Forest1):1,]
+                     )
+
+
 Forest2 <- Terr2[Terr2[,1] < 255,]
 Forest2poly <- rbind(Forest2 + data.frame(rep(0,nrow(Forest2)), rep(1, nrow(Forest2))),
                      Forest2[nrow(Forest2):1,]
@@ -31,24 +38,13 @@ plot(Terr1, axes = FALSE, type = "n",
 axis(1, lwd = 0.4)
 axis(1, at = c(0,6271.4), labels = NA, tck = 0, lwd = 0.5)
 axis(2, lwd = 0.5)
-## Forest
-lines(Terr1[Terr1[,1] > 1981 & Terr1[,1] < 3600,] + 0.26,
-      lwd = 11, col = "forestgreen")
-lines(Terr1[Terr1[,1] > 1981 & Terr1[,1] < 3600,] + 0.08,
-      lwd = 3, col = "forestgreen")
-## Lower white mask
-lines(Terr1[Terr1[,1] > 1981 & Terr1[,1] < 3600,]- 0.13, col = "white", lwd = 4)
-lines(Terr1[Terr1[,1] > 1981 & Terr1[,1] < 3600,]- 0.28, col = "white", lwd = 6)
-lines(c(1975,1975),c(88,89), col = "white", lwd = 6)
 ## Soil background
 polygon(rbind(Terr1, data.frame(x=c(6271.4,0),y=c(0,0))), col = "#BD9A7A", border = NA)
 grid(ny = NA, col="lightgray", lty = 1)
 lines(rep(c(0, 6271.4,NA),6), rep(86:91, each = 3), col="lightgray", lty = 1)
-## Forest patch
-lines(c(3000,3000),c(89,89.85), col = "forestgreen", lwd = 1.5)
-lines(c(2160,2219),c(89,89), col = "forestgreen", lwd = 1.5, lend = 1)
-lines(c(3550,3620),c(89,89), col = "forestgreen", lwd = 1.5, lend = 1)
-lines(c(3064,3135),c(90,90), col = "forestgreen", lwd = 1.5)
+lines(Forest1poly[3:nrow(Forest1)-1,], lwd = 5, col = "forestgreen")
+polygon(Forest1poly,
+        col = "forestgreen", border = NA)
 ## Terrain
 lines(Terr1, lwd = 3, 
       col = SoilColor
