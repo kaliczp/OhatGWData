b4dfd2bc787b9ecf9090ac427c8c50d73b24a3ed
Kalicz Péter <kaliczp@gmail.com>
2025-07-03 14:01:25 +0200

    Import Fig5 data

---
+++ Fig5terrainGW.R
@@ -0,0 +1,6 @@
+SoilColor <- #92745B
+library(readxl)
+Terr1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "A2:B249"))
+GW1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "D1:N125"))
+GW1 <- GW1[, -c(3:5,9)] # Remove repeated and empty cols
+
