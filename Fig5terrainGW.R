4234212e81fe419cde92c6cb13d89903f5c023fb
Kalicz Péter <kaliczp@gmail.com>
2025-07-03 14:49:48 +0200

    Corrcted terrain import

--- Fig5terrainGW.R
+++ Fig5terrainGW.R
@@ -1,6 +1,6 @@
 SoilColor <- "#92745B"
 library(readxl)
-Terr1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "A2:B249"))
+Terr1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "A2:B249", col_names = FALSE))
 GW1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "D1:N125"))
 GW1 <- GW1[, -c(3:5,9)] # Remove repeated and empty cols
 
