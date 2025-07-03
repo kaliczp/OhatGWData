681c9a91a80aa64c6b928c024694a361358f8722
Kalicz Péter <kaliczp@gmail.com>
2025-07-03 14:50:14 +0200

    First reproduced figure

--- Fig5terrainGW.R
+++ Fig5terrainGW.R
@@ -4,3 +4,19 @@ Terr1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "A2:B249
 GW1 <- as.data.frame(read_excel("GW_section_2025_KPnek.xlsx", range = "D1:N125"))
 GW1 <- GW1[, -c(3:5,9)] # Remove repeated and empty cols
 
+
+plot(Terr1, axes = FALSE, type = "n",
+     ylim = c(85.5, 90),
+     xlab = "", ylab = "",
+     xaxs = "i"
+     )
+## Terrain
+lines(Terr1, lwd = 2, 
+      col = SoilColor
+      )
+## Ref
+lines(GW1[,1],GW1[,2], lwd = 2, col = GWcolors["reference"])
+## Passive
+lines(GW1[,1],GW1[,3], lwd = 2, col = GWcolors["passive"])
+## Active
+lines(GW1[,1],GW1[,6], lwd = 2, col = GWcolors["active"])
