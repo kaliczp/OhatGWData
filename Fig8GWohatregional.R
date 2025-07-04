b53d5cbeba56d7ec05ed665fb4143a7415ae7182
Kalicz Péter <kaliczp@gmail.com>
2025-07-04 15:32:30 +0200

    Pdf with layout and dummy plots

--- Fig8GWohatregional.R
+++ Fig8GWohatregional.R
@@ -4,13 +4,25 @@ OhatGWreg <- as.data.frame(read_excel("Adatok-FigS5Fig10AFig11.xlsx"))
 library(xts)
 GWreg <- xts(as.numeric(OhatGWreg[-(1:3),"FIG10A"]), as.Date(OhatGWreg[-(1:3), 1]))
 
-par(las = 1, lwd = 0.5)
-plot.zoo(GWreg['1979/'],
+Ohat1.mnth <- xts(as.numeric(OhatGWreg[735:761,11]), as.Date(OhatGWreg[735:761,10])) 
+
+pdf("Fig8GWohatregional.pdf", width = 19/2.54, height = 12/2.54, pointsize = 9)
+layout(matrix(c(1, 1, 2, 3), nrow = 2, byrow = TRUE))
+par(las = 1, lwd = 0.5, mar = c(0,0,0,0), oma = c(2.6,2.6,2.6,0.2))
+plot.zoo(GWreg['1978-12/'],
+         type = "n", axes = FALSE,
          lwd = 2, col = GWcolors["active"],
          ylim = c(-6,0),
          xaxs = "i", yaxs = "i",
-         xaxt = "n",
          xlab = "", ylab = "GW [m]"
          )
-axis.Date(3, tck = 0)
-axis(3, at = as.Date(paste0(1979:2023, "-01-01")), labels = NA)
+axis(2, lwd = 0.5)
+axis.Date(3, lwd = 0.4)
+axis(3, at = as.Date(paste0(1979:2023, "-01-01")), labels = NA, tcl = -0.3, lwd = 0.5)
+lines(as.zoo(GWreg['1978-12/']),
+         lwd = 2, col = GWcolors["active"],
+         )
+lines(as.zoo(Ohat1.mnth), lwd = 3, col = GWcolors["passive"])
+plot(c(0,1), c(0,1), axes = FALSE, type = "l")
+plot(c(0,1), c(0,1), axes = FALSE, type = "l")
+dev.off()
